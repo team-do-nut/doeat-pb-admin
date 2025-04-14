c30a7915f41120006aff785c0b02be5801f7cb87
ghh237 <ghh237@naver.com>
2025-04-14 15:59:51 +0900

    chore: emotion + @tanstack/react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import "@src/styles/globals.css";
-import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import type { AppProps } from 'next/app';
+import GlobalStyles from '@src/styles/GlobalStyles';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import '@src/styles/reset.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: 60 * 3 * 1000,
+      gcTime: 60 * 5 * 1000,
+    },
+  },
+});
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <QueryClientProvider client={queryClient}>
+    <GlobalStyles />
+    <Component {...pageProps} />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
+
+export default App;
