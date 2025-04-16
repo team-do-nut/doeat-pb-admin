5e6727938b713f2fe3e739497060d94a70576f29
ghh237 <ghh237@naver.com>
2025-04-16 15:35:03 +0900

    fix: lint 에러

--- src/components/Navigation/styles.tsx
+++ src/components/Navigation/styles.tsx
@@ -1,5 +1,5 @@
-import styled from '@emotion/styled';
 import Link from 'next/link';
+import styled from '@emotion/styled';
 
 export const NavContainer = styled.nav`
   display: flex;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import type { AppProps } from 'next/app';
 import GlobalStyles from '@src/styles/GlobalStyles';
 
 import '@src/styles/reset.css';
